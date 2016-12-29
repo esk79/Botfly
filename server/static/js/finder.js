7878dccf693dd4b846218c09f104f9136a1a9599
Evan King <esk79@cornell.edu>
2016-12-29 12:49:01 -0800

    added search bar

--- server/static/js/finder.js
+++ server/static/js/finder.js
@@ -8,7 +8,7 @@ var filemanager = $('.filemanager'),
     fileList = filemanager.find('.data');
 
 
-function generateFileFolderObject(response) {
+function generateFileFolderObject(response, search) {
     //get dictionary portion of response
     response = response[1]
 
@@ -21,11 +21,13 @@ function generateFileFolderObject(response) {
             var pathParts = path.split("/")
             var name = pathParts[pathParts.length - 1]
 
-            if (isFolder) {
-                scannedFolders.push({"name": name, "type": "folder", "path": path, "items": null})
-            }
-            else {
-                scannedFiles.push({"name": name, "type": "file", "path": path, "size": size})
+            if (search == null || name.toLowerCase().includes(search.toLowerCase())) {
+                if (isFolder) {
+                    scannedFolders.push({"name": name, "type": "folder", "path": path, "items": null})
+                }
+                else {
+                    scannedFiles.push({"name": name, "type": "file", "path": path, "size": size})
+                }
             }
 
         }
@@ -34,10 +36,11 @@ function generateFileFolderObject(response) {
 }
 
 
-function generateFileFolderIcons(response) {
+function generateFileFolderIcons(response, search) {
     fileList.empty();
+    search = search || null;
 
-    formattedObjects = generateFileFolderObject(response)
+    formattedObjects = generateFileFolderObject(response, search)
     scannedFiles = formattedObjects[0]
     scannedFolders = formattedObjects[1]
     // Empty the old result and make the new one
@@ -86,33 +89,24 @@ function generateBreadcrumbsIcon(response) {
 
     var url = '';
 
-    if (filemanager.hasClass('searching')) {
 
-        url = '<span>Search results: </span>';
-        fileList.removeClass('animated');
+    fileList.addClass('animated');
+    url += '<a onclick="getLS(\'/\')"><span class="bread folderName">/</span></a>';
 
-    }
-    else {
+    breadcrumbsUrls.forEach(function (path, i) {
 
-        fileList.addClass('animated');
-        url += '<a onclick="getLS(\'/\')"><span class="bread folderName">/</span></a>';
-
-        breadcrumbsUrls.forEach(function (path, i) {
-
-            var name = path.split('/');
+        var name = path.split('/');
 
 
-            if (i !== breadcrumbsUrls.length - 1) {
-                console.log("here")
-                url += '<a onclick="getLS(\'' + path + '\')"><span class="bread folderName">' + name[name.length - 1] + '</span></a> <span class="arrow">→</span> ';
-            }
-            else {
-                url += '<span class="folderName">' + name[name.length - 1] + '</span>';
-            }
+        if (i !== breadcrumbsUrls.length - 1) {
+            url += '<a onclick="getLS(\'' + path + '\')"><span class="bread folderName">' + name[name.length - 1] + '</span></a> <span class="arrow">→</span> ';
+        }
+        else {
+            url += '<span class="folderName">' + name[name.length - 1] + '</span>';
+        }
 
-        });
+    });
 
-    }
 
     breadcrumbs.text('').append(url);
 
@@ -122,204 +116,54 @@ function generateBreadcrumbsIcon(response) {
     fileList.animate({'display': 'inline-block'});
 }
 
-//render finder with server response
-function renderFinder(response) {
-    generateFileFolderIcons(response);
-    generateBreadcrumbsIcon(response);
-}
+function generateSearchBar(response) {
+    // Hiding and showing the search box
+    filemanager.find('.search').click(function () {
 
-// This function escapes special html characters in names
-function escapeHTML(text) {
-    return text.replace(/\&/g, '&amp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
-}
+        var search = $(this);
 
-// Convert file sizes from bytes to human readable units
-function bytesToSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-    if (bytes == 0) return '0 Bytes';
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
-}
+        search.find('span').hide();
+        search.find('input[type=search]').show().focus();
 
-// Splits a file path and turns it into clickable breadcrumbs
-function generateBreadcrumbsPath(nextDir) {
-    var path = nextDir.split('/').slice(0);
-    for (var i = 1; i < path.length; i++) {
-        path[i] = path[i - 1] + '/' + path[i];
-    }
-    return path;
-}
-
-//post call to server to get LS info from bot
-function getLS(path) {
-    $.ajax({
-        type: "POST",
-        url: "/ls",
-        data: {
-            file: path
-        },
-        success: function (data) {
-            console.log(data)
-        }
     });
-}
 
-// download file from connected bot
-function downloadFile(filename) {
-    $.ajax({
-        type: "POST",
-        url: "/downloader",
-        data: {
-            file: filename
-        },
-        success: function (data) {
-            var blob = new Blob([data]);
-            var link = document.createElement('a');
-            link.href = window.URL.createObjectURL(blob);
-            var pathParts = filename.split("/")
-            link.download = pathParts[pathParts.length - 1]
-            link.click();
-        }
+    // Listening for keyboard input on the search field.
+    // We are using the "input" event which detects cut and paste
+    // in addition to keyboard input.
+    filemanager.find('input').on('input', function (e) {
+        var value = this.value.trim();
+        console.log(value)
+        generateFileFolderIcons(response, value)
     });
+    //}).on('keyup', function (e) {
+    //
+    //    // Clicking 'ESC' button triggers focusout and cancels the search
+    //    var search = $(this);
+    //    if (e.keyCode == 27) {
+    //        search.trigger('focusout');
+    //    }
+    //
+    //}).focusout(function (e) {
+    //
+    //    // Cancel the search
+    //    var search = $(this);
+    //    if (!search.val().trim().length) {
+    //        search.hide();
+    //        search.parent().find('span').show();
+    //    }
+    //
+    //});
+
 }
 
-$(document).ready(function () {
-    getLS('.')
-    //handle response emitted by server
-    socket.on('finder', function (msg) {
-        if (msg.special.hasOwnProperty('ls')) {
-            var response = JSON.parse(msg.special['ls']);
-            renderFinder(response);
-        }
-    });
-});/**
- * Created by EvanKing on 12/28/16.
- */
-
-
-var filemanager = $('.filemanager'),
-    breadcrumbs = $('.breadcrumbs'),
-    fileList = filemanager.find('.data');
-
-
-function generateFileFolderObject(response) {
-    //get dictionary portion of response
-    response = response[1]
-
-    scannedFiles = []
-    scannedFolders = []
-    for (var path in response) {
-        if (response.hasOwnProperty(path)) {
-            var isFolder = response[path][0];
-            var size = response[path][2]
-            var pathParts = path.split("/")
-            var name = pathParts[pathParts.length - 1]
-
-            if (isFolder) {
-                scannedFolders.push({"name": name, "type": "folder", "path": path, "items": null})
-            }
-            else {
-                scannedFiles.push({"name": name, "type": "file", "path": path, "size": size})
-            }
-
-        }
-    }
-    return [scannedFiles, scannedFolders]
-}
-
-
-function generateFileFolderIcons(response) {
-    fileList.empty();
-
-    formattedObjects = generateFileFolderObject(response)
-    scannedFiles = formattedObjects[0]
-    scannedFolders = formattedObjects[1]
-    // Empty the old result and make the new one
-
-    if (!scannedFolders.length && !scannedFiles.length) {
-        filemanager.find('.nothingfound').show();
-    }
-    else {
-        filemanager.find('.nothingfound').hide();
-    }
-
-    if (scannedFolders.length) {
-
-        scannedFolders.forEach(function (f) {
-            var name = escapeHTML(f.name),
-                icon = '<span class="icon folder"></span>';
-            var folder = $('<li class="folders"><a onclick="getLS(\'' + f.path + '\')" title="' + f.path + '" class="folders">' + icon + '<span class="name">' + name + '</span> </a></li>');
-            folder.appendTo(fileList);
-        });
-
-    }
-
-    if (scannedFiles.length) {
-
-        scannedFiles.forEach(function (f) {
-
-            var fileSize = bytesToSize(f.size),
-                name = escapeHTML(f.name),
-                fileType = name.split('.'),
-                icon = '<span class="icon file"></span>';
-
-            fileType = fileType[fileType.length - 1];
-
-            icon = '<span class="icon file f-' + fileType + '">.' + fileType + '</span>';
-
-            var file = $('<li class="files"><a onclick="downloadFile(\'' + f.path + '\')" title="' + f.path + '" class="files">' + icon + '<span class="name">' + name + '</span> <span class="details">' + fileSize + '</span></a></li>');
-            file.appendTo(fileList);
-        });
-
-    }
-}
-
-function generateBreadcrumbsIcon(response) {
-    // Generate the breadcrumbs
-    breadcrumbsUrls = generateBreadcrumbsPath(response[0])
-
-    var url = '';
-
-    if (filemanager.hasClass('searching')) {
-
-        url = '<span>Search results: </span>';
-        fileList.removeClass('animated');
-
-    }
-    else {
-
-        fileList.addClass('animated');
-        url += '<a onclick="getLS(\'/\')"><span class="bread folderName">/</span></a>';
-
-        breadcrumbsUrls.forEach(function (path, i) {
-
-            var name = path.split('/');
-
-
-            if (i !== breadcrumbsUrls.length - 1) {
-                console.log("here")
-                url += '<a onclick="getLS(\'' + path + '\')"><span class="bread folderName">' + name[name.length - 1] + '</span></a> <span class="arrow">→</span> ';
-            }
-            else {
-                url += '<span class="folderName">' + name[name.length - 1] + '</span>';
-            }
-
-        });
-
-    }
-
-    breadcrumbs.text('').append(url);
-
-
-    // Show the generated elements
-
-    fileList.animate({'display': 'inline-block'});
-}
 
 //render finder with server response
 function renderFinder(response) {
+    var value = $('.div.search.input-group')
+    console.log(value)
     generateFileFolderIcons(response);
     generateBreadcrumbsIcon(response);
+    generateSearchBar(response);
 }
 
 // This function escapes special html characters in names
