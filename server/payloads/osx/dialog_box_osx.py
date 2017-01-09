a776694b20b90301e88877d395d2394fe9c67439
Evan King <esk79@cornell.edu>
2017-01-09 13:58:59 -0800

    added button option to dialog payload

--- server/payloads/osx/dialog_box_osx.py
+++ server/payloads/osx/dialog_box_osx.py
@@ -2,11 +2,17 @@
 NAME: Dialog Box
 DESCRIPTION: Pops up a native dialog box with the provided DIALOG text
 VAR DIALOG: The text that will populate the pop-up (ie, "I'm watching you...")
+VAR BUTTONS = OK: Add optional buttons to pop-up. Separate buttons with commas (ie, "Yes,No,Maybe") defaults to "OK"
 '''
 import subprocess
 import sys
 
-script = 'tell app "System Events" to display dialog "{}"'.format(DIALOG)
+
+#ugly but chaining is actually proven best way to do multiple replace
+buttons  = str(BUTTONS.split(',')).replace('[', '{').replace(']', '}').replace('\'', '"')
+buttons = 'buttons ' + buttons
+
+script = 'tell app "System Events" to display dialog "{}" {}'.format(DIALOG, buttons)
 proc = subprocess.Popen(['osascript', '-'],
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
